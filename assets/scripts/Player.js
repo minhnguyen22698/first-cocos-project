a5fcbec298f2f92b28515427f15627d2a6dd3a2b
minhnguyen22698 <minhnguyen22698@gmail.com>
2021-04-14 11:20:03 +0700

    first commit

---
+++ assets/scripts/Player.js
@@ -0,0 +1,104 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // Main character's jump height
+        jumpHeight: 0,
+        // Main character's jump duration
+        jumpDuration: 0,
+        // Maximal movement speed
+        maxMoveSpeed: 0,
+        // Acceleration
+        accel: 0,
+
+    },
+
+    runJumpAction() {
+        // Jump up
+        var jumpUp = cc.tween().by(this.jumpDuration, { y: this.jumpHeight }, { easing: 'sineOut' });
+        // Jump down
+        var jumpDown = cc.tween().by(this.jumpDuration, { y: -this.jumpHeight }, { easing: 'sineIn' });
+
+        // Create a easing and perform actions in the order of "jumpUp", "jumpDown"
+        var tween = cc.tween().sequence(jumpUp, jumpDown);
+        // Repeat
+        return cc.tween().repeatForever(tween);
+    },
+
+    //Player move
+    onKeyDown (event) {
+        // Set a flag when key pressed
+        switch(event.keyCode) {
+            case cc.macro.KEY.a:
+                this.accLeft = true;
+                break;
+            case cc.macro.KEY.d:
+                this.accRight = true;
+                break;
+        }
+    },
+    onKeyUp (event) {
+        // Unset a flag when key released
+        switch(event.keyCode) {
+            case cc.macro.KEY.a:
+                this.accLeft = false;
+                break;
+            case cc.macro.KEY.d:
+                this.accRight = false;
+                break;
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad() {
+        //Khoi tao jump
+        var jumpAction = this.runJumpAction();
+        cc.tween(this.node).then(jumpAction).start()
+
+        //Khoi tao huong chuyen doi
+        this.accLeft = false;
+        this.accRight = false;
+
+        this.xSpeed = 0;
+
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+
+
+    },
+    onDestroy () {
+        // Cancel keyboard input monitoring
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    },
+
+    start() {
+
+    },
+
+    update: function (dt) {
+        console.log(dt)
+        // Update speed of each frame according to the current acceleration direction
+        if (this.accLeft) {
+            this.xSpeed -= this.accel * dt;
+        } else if (this.accRight) {
+            this.xSpeed += this.accel * dt;
+        }
+        // Restrict the movement speed of the main character to the maximum movement speed
+        if ( Math.abs(this.xSpeed) > this.maxMoveSpeed ) {
+            // If speed reach limit, use max speed with current direction
+            this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+        }
+
+        // Update the position of the main character according to the current speed
+        this.node.x += this.xSpeed * dt;
+    },
+});
---
+++ temp/quick-scripts/assets/scripts/Star.js
@@ -0,0 +1,69 @@
+(function() {"use strict";var __module = CC_EDITOR ? module : {exports:{}};var __filename = 'preview-scripts/assets/scripts/Star.js';var __require = CC_EDITOR ? function (request) {return cc.require(request, require);} : function (request) {return cc.require(request, __filename);};function __define (exports, require, module) {"use strict";
+cc._RF.push(module, '6731dIoLQhHybPbzVWc5P2N', 'Star', __filename);
+// scripts/Star.js
+
+"use strict";
+
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        pickRadius: 4
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+    getPlayerDistance: function getPlayerDistance() {
+        // Determine the distance according to the position of the Player node
+        var playerPos = this.game.player.getPosition();
+
+        // Calculate the distance between two nodes according to their positions
+        var dist = this.node.position.sub(playerPos).mag();
+        return dist;
+    },
+
+    onPicked: function onPicked() {
+        // When the stars are being collected, invoke the interface in the Game script to generate a new star
+        this.game.spawnNewStar();
+
+        // Then destroy the current star's node
+        this.node.destroy();
+    },
+
+    start: function start() {},
+
+
+    update: function update(dt) {
+        // Determine if the distance between the Star and main character is less than the collecting distance for each frame
+        if (this.getPlayerDistance() < this.pickRadius) {
+            // Invoke collecting behavior
+            this.onPicked();
+            return;
+        }
+    }
+});
+
+cc._RF.pop();
+        }
+        if (CC_EDITOR) {
+            __define(__module.exports, __require, __module);
+        }
+        else {
+            cc.registerModuleFunc(__filename, function () {
+                __define(__module.exports, __require, __module);
+            });
+        }
+        })();
+        //# sourceMappingURL=Star.js.map
+        
